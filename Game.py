be03e36e1e75b0dd3796dfd9f7e623efc9a214bc
Francesco Maresca <97321140+programmerhelloworld@users.noreply.github.com>
2025-02-02 23:08:39 +0100

    Version 0.03 Rev. 1

--- Game.py
+++ Game.py
@@ -101,8 +101,8 @@ def save_world():
         except Exception as e:
             print(f"Error saving world: {e}")
 
-def getHeight(): # Added this function in order to fix the falling glitch when loading world, which gets a correct height where the player can spawn
-    for x in range(-world_size, world_size):
+def getHeight():                                                        # Added this function in order to fix the falling glitch when loading world,                      
+    for x in range(-world_size, world_size):                            # which gets a correct height where the player can spawn
         for z in range(-world_size, world_size):
             height = int(noise([x / 10, z / 10]) * 5)
     return height
@@ -139,12 +139,10 @@ def load_world():
                     position = tuple(block_data["position"])
                     block_type = block_data["block_type"]
                     add_block(position, block_type)
-                player.position = (0, plH, 0) # If removed, when loading a new world collision will bug and player will fall
+                player.position = (0, plH, 0)                           # If removed, when loading a new world collision will bug and player will fall
             print(f"World loaded from {file_path}!")
         except Exception as e:
             print(f"Error loading world: {e}")
-
-#camera.clip_plane_far = 10
                 
 
 def add_block(position, block_type):
@@ -166,7 +164,7 @@ class Block(Entity):
 player = FirstPersonController(
     name = "Player",
     mouse_sensitivity=Vec2(100, 100),
-    position=(0, plH+2, 0)  # Adding two blocks height in order to be sure that spawns on air and not inside blocks (colliding bug needs to be fixed ASAP)
+    position=(0, plH+2, 0)                                              # Adding two blocks height in order to be sure that spawns on air and not inside blocks (colliding bug needs to be fixed ASAP)
 )
 
 # Mini block 
@@ -198,7 +196,7 @@ def input(key):
     elif key == 'g':
         player.position = (1, 12, 1)
         load_world()
-    elif key == 'f2': # Basically freezes the game, thinking of including a real pause menu someday
+    elif key == 'f2':                                                   # Basically freezes the game, thinking of including a real pause menu someday
         if (player.enabled == True):
             player.enabled = False
             pauseText.visible = True
@@ -237,12 +235,12 @@ def update():
         player.y = plH+2
         player.x -= 1
         player.y -= 1
-menu_mode = True  # Defines the state of the game
+menu_mode = True                                                        # Defines the state of the game
 if menu_mode == True:
     logo = Sprite("miscellaneous/logo.png", parent=camera.ui, position=(0,0.25), scale = 0.07)
     logo.enabled = True
-    player.enabled = False  # Player, when in the menu, not able to play
-    playerName.enabled = True  # Shows the player inputField
+    player.enabled = False                                              # Player, when in the menu, not able to play
+    playerName.enabled = True                                           # Shows the player inputField
 
 def nameAuthentication():
     player.name = playerName.text
@@ -260,9 +258,9 @@ def setMenuMode():
     menu_mode = False
     playerName.enabled = False
     print("Play")
-    player.enabled = True  # Enable the player to start the game
-    playButton.enabled = False  # Disable the play button after clicking
-    player.position = (0, plH + 2, 0)  # Make sure the player starts in a valid position
+    player.enabled = True                                               # Enable the player to start the game
+    playButton.enabled = False                                          # Disable the play button after clicking
+    player.position = (0, plH + 2, 0)                                   # Make sure the player starts in a valid position
     
     # Block bar
     bar_position = Vec3(0, -0.42, 0)
@@ -278,7 +276,7 @@ def setMenuMode():
             texture=block_textures[block_type],
             scale=(0.1, 0.1),
             position=(start_position_x + (i * 0.12), bar_position.y),
-            on_click=lambda i=i: update_selected_block(i)  # Fix closure by using i as a default argument
+            on_click=lambda i=i: update_selected_block(i)               # Fix closure by using i as a default argument
         ))
 
 
